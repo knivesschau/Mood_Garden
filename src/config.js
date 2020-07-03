f24c1888f43656faf7b60b381cc6de0ef4bdb500
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-07-02 23:26:15 -0700

    adjusted config for local environment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://calm-coast-19093.herokuapp.com/api',
+    API_ENDPOINT: 'http://localhost:8000/api', //'https://calm-coast-19093.herokuapp.com/api'//,
     TOKEN_KEY: 'mood-garden-client-auth-token',
 }
